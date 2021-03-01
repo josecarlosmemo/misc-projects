9566a95aca182baed1e086f5f5fd57b45000ba72
josecarlosmemo <misterio205.1@gmail.com>
2021-02-28 21:32:07 -0600

    Added pyqt5 main window

--- wii-game-manager/main.py
+++ wii-game-manager/main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from PyQt5.QtWidgets import *
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def loadUI(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    window.show()
+
+    sys.exit(app.exec_())
+
